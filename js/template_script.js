a8c466802e6f214bdac3033777c2d874e50b0a41
alexander <al-programmist@yandex.ru>
2018-08-22 13:05:25 +0800

    #add: Валидация работает

--- js/template_script.js
+++ js/template_script.js
@@ -11,30 +11,56 @@ $(document).ready(function () {
 
 	//Раздел функций
 	//------------------------------------------------------------------------------------------------------
-	$('.user-telephone').mask('PP (YYY) YYY YY YY',{
-		'translation': {
-			P: {pattern: /[\+7]/},
-			Y: {pattern: /[0-9]/},
-		
+	initFields($registrationFormsInput);
+	$('.user-telephone').mask('+7 (000) 000-00-00');
+	$('.callback-form').validate({
+	
+		onkeyup: function(element){
+			var $wrapper = $(element).parent();
+			$(element).valid(); 
+			if($(element).hasClass('valid')) {
+	
+				$wrapper.addClass('input-ok');
+				$wrapper.removeClass('input-error');
+			}
+			if(!$(element).hasClass('valid')) {
+				$wrapper.addClass('input-error');
+				$wrapper.removeClass('input-ok');
+			}
+			if($(element).val().length <= 0) {
+				$wrapper.removeClass('input-ok');
+				$wrapper.removeClass('input-error');
+			}
 		}
+	});
+
+	$.validator.addClassRules({
+		utl: {
+		  required: true,
+		  minlength: 18
+		}
+	
 	  });
+
+
 
 	  $registrationFormsInput.on('focus', function () {
 		var $wrapper = $(this).parent(),
-			$wrappers = $registrationFormsInput.parent();
-		$.each($wrappers, function (indexInArray, valueOfElement) { 
-			$(this).removeClass('active');
-		});
+			$wrappers = $registrationFormsInput.parent(),
+			value;
+		value = clearValidationClasses($wrappers, value);
 		$wrapper.addClass('active');
 		
 	
 	});
 
 	$registrationFormsInput.on('blur', function () {
-		var $wrappers = $registrationFormsInput.parent();
+		var $wrappers = $registrationFormsInput.parent(),
+		value;
 		$.each($wrappers, function (indexInArray, valueOfElement) { 
 			$(this).removeClass('active');
 		});
+		clearValidationClasses($wrappers, value);
 		
 		
 	
@@ -52,10 +78,21 @@ $(document).ready(function () {
 			if(value.length <= 0) {
 				$wrapper.removeClass('filled');
 			}
+			
 		
+	});
+
+	$registrationFormPlaceholders.click(function (e) { 
+		e.preventDefault();
+		var $wrapper = $(this).parent(),
+		$wrappers = $registrationFormsInput.parent(),
+		$activeField = $(this).siblings('input');
+		$.each($wrappers, function (indexInArray, valueOfElement) { 
+			$(this).removeClass('active');
+		});
+		$wrapper.addClass('active');
+		$activeField.focus();
 		
-		
-	
 	});
    
 	//Активация медиа-запросов в javascript
@@ -86,6 +123,18 @@ $(document).ready(function () {
 
 
 
+function clearValidationClasses($wrappers, value) {
+	$.each($wrappers, function (indexInArray, valueOfElement) {
+		$(this).removeClass('active');
+		value = String($(this).children('input').val()).trim();
+		if (value.length <= 0) {
+			$(this).removeClass('input-ok');
+			$(this).removeClass('input-error');
+		}
+	});
+	return value;
+}
+
 function media(mediaQueryString, action){
 	'use strict';
 	var handleMatchMedia = function (mediaQuery) {
@@ -100,4 +149,18 @@ function media(mediaQueryString, action){
 	mql.addListener(handleMatchMedia);
 }
 
+function initFields($registrationFormsInput) {
+	var value='',
+		$wrapper;
+	$.each($registrationFormsInput, function (indexInArray, valueOfElement) { 
+		value =  String($(this).val()).trim();
+		$wrapper = $(this).parent();
+		if(value.length > 0) {
+			$wrapper.addClass('filled');
+		}
 
+		if(value.length <= 0) {
+			$wrapper.removeClass('filled');
+		}
+	});
+}
